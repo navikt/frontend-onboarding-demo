97483c5adc3cd33b97b2e53b1c91f08d3d71fc3a
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2023-05-30 13:28:18 +0200

    :lipstick: Legger til designsystem

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Heading } from "@navikt/ds-react";
+
+export default function Index() {
+  return (
+    <main>
+      <Heading level="1" size="xlarge">
+        Frontend onboarding demo
+      </Heading>
+    </main>
+  );
+}
